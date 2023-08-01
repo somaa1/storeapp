d36f46c0fb4bf8272456ff78e8fd78e72517cf4f
somaa1 <ismailrabie111@gmail.com>
2023-08-01 18:13:54 +0300

    .

--- lib/core/resources/manager_strings.dart
+++ lib/core/resources/manager_strings.dart
@@ -1,4 +1,16 @@
 class ManagerStrings {
   static const String appName = 'اذكار';
   static const String aboutScreen = 'About Screen';
+  static const String id = 'id';
+  static const String title='title';
+  static const String price='price';
+  static const String description='description';
+  static const String image='image';
+  static const String rate='rate';
+  static const String count='count';
+  static const String rating='rating';
+static const String baseuriApi='https://fakestoreapi.com';
+
+
+
 }
