b567e00aaa62ef382a5c35c7c5f4b6c8bdc0b601
somaa1 <ismailrabie111@gmail.com>
2023-08-01 20:34:41 +0300

    create class all category and fetch data from api

--- lib/services/all_categories.dart
+++ lib/services/all_categories.dart
@@ -1,3 +1,16 @@
-class AllCatgories{
+import 'dart:convert';
 
+import '../core/resources/manager_strings.dart';
+import 'package:http/http.dart' as http;
+
+import '../helper/api.dart';
+
+class AllCatgories {
+  Future<List<dynamic>> GetAllCategory() async {
+    List<dynamic> data = await Api().get(
+        uri: '${ManagerStrings.baseuriApi}/products/categories');
+
+    return data;
+
+  }
 }
