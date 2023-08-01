cf4f25e7df3a0772e340f6ee9a924a9473e55af0
somaa1 <ismailrabie111@gmail.com>
2023-08-01 20:37:57 +0300

    edit some code

--- lib/services/get_all_product_service.dart
+++ lib/services/get_all_product_service.dart
@@ -2,15 +2,15 @@ import 'dart:convert';
 
 import 'package:storeapp/core/resources/manager_strings.dart';
 
+import '../helper/api.dart';
 import '../models/proudct_model.dart';
 import 'package:http/http.dart' as http;
 
 class GetAllProudcts {
   Future<List<ProductModel>> getAllProudct() async {
-    http.Response response =
-        await http.get(Uri.parse(ManagerStrings.baseuriApi)); //رابط الاي بي اي
+    List<dynamic> data =
+        await Api().get(uri: '${ManagerStrings.baseuriApi}/products');
 
-    List<dynamic> data = jsonDecode(response.body); //ترجيع كل الداتا
     List<ProductModel> productsList = []; // انشاء ليست للبرودكت
     for (int i = 0; i < data.length; i++) {
       productsList.add(ProductModel.fromJson(data[i])); //تخزينهم
