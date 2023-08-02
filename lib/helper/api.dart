109f41f48f22cea25b688c291575f77a3c37057d
somaa1 <ismailrabie111@gmail.com>
2023-08-02 20:51:07 +0300

    edit some code and create post and put method

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,14 +1,64 @@
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String uri}) async {
-    http.Response response = await http.get(Uri.parse(uri));
+  Future<dynamic> get({required String uri, @required String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(Uri.parse(uri), headers: headers);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
       throw Exception('problem in statusCode ${response.statusCode}');
     }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(
+          'problem in statusCode ${response.statusCode} with body ${response.body}');
+    }
+  }
+}
+
+Future<dynamic> put(
+    {required String url,
+    @required dynamic body,
+    @required String? token}) async {
+  Map<String, String> headers = {};
+  headers.addAll({
+    'Content-Type': 'application/x-www-form-urlencoded',
+  });
+  if (token != null) {
+    headers.addAll({'Authorization': 'Bearer $token'});
+  }
+  http.Response response =
+      await http.post(Uri.parse(url), body: body, headers: headers);
+  if (response.statusCode == 200) {
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    return data;
+  } else {
+    throw Exception(
+        'problem in statusCode ${response.statusCode} with body ${response.body}');
   }
 }
