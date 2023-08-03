f2ebe46523032b46a66c07802378b6e21e4c3451
somaa1 <ismailrabie111@gmail.com>
2023-08-03 16:45:30 +0300

    create routes in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:storeapp/core/routes/routes.dart';
+import 'package:storeapp/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StoreApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(child: Text("safafs"),),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: routes.Home  ,
+      routes: {
+   routes.Home : (context)=> HomePage(),
+
+      }
     );
   }
 }
