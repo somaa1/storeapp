95d78828d61a1f314e45a98d2e8981a4a84bdf49
somaa1 <ismailrabie111@gmail.com>
2023-08-04 17:24:41 +0300

    create update product

---
+++ lib/pages/update_product.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class UpdateProduct extends StatefulWidget {
+  const UpdateProduct({super.key});
+
+  @override
+  State<UpdateProduct> createState() => _UpdateProductState();
+}
+
+class _UpdateProductState extends State<UpdateProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
